4f98c9d2243f1e5b437dbba7527a36dcc2286f51
emmanouil e <emmanouil.e25@gmail.com>
2017-09-09 11:01:31 +0100

    pom  and readme.md changes

--- spring5-recipe-app/src/main/java/com/manos/spring5recipeapp/services/RecipeServiceImpl.java
+++ spring5-recipe-app/src/main/java/com/manos/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.manos.spring5recipeapp.services;
 
 import com.manos.spring5recipeapp.Repositories.RecipeRepository;
 import com.manos.spring5recipeapp.models.Recipe;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -15,6 +17,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public ArrayList<Recipe> getAllRecipes() {
+
+        log.debug("i am in the service");
         return (ArrayList<Recipe>) recipeRepository.findAll();
     }
 }
